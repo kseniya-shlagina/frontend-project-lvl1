52bd8a624f29ff56084e0ef5eca31cd61e725631
Kseniya <kseniya.shlagina@gmail.com>
2019-11-14 17:42:00 +0700

    Revert "trigger build fail"
    This reverts commit 1a28c7300709bd23189738436839d57e1185a6bf.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,4 @@ import readlineSync from 'readline-sync';
 export const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-      
 };
