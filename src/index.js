1a28c7300709bd23189738436839d57e1185a6bf
kseniya-shlagina <50040590+kseniya-shlagina@users.noreply.github.com>
2019-11-14 17:39:19 +0700

    trigger build fail

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import readlineSync from 'readline-sync';
 export const getName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+      
 };
