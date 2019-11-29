49b526434ac7ecd751737b3df8170c8332a2e213
Kseniya <kseniya.shlagina@gmail.com>
2019-11-29 17:22:36 +0700

    Eighth step, brain-progression step

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ How to run brain-gcd game:
 
 https://asciinema.org/a/60iSoHkWGVqBbfUaJNEKYjmKt
 
+How to run brain-progression game:
+
+https://asciinema.org/a/BoIQ4dsDsYjYAivhdAoySyf85
+
 <a href="https://codeclimate.com/github/kseniya-shlagina/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/218681c7780d2721d291/maintainability" /></a>
 
 [![Build Status](https://travis-ci.com/kseniya-shlagina/frontend-project-lvl1.svg?branch=master)](https://travis-ci.com/kseniya-shlagina/frontend-project-lvl1)
---
+++ src/games/brain-progression-game.js
@@ -0,0 +1,31 @@
+import { randomNumber } from '../utils';
+
+export const brainGameRule = 'What number is missing in the progression?';
+
+const makeQuestion = (num) => {
+  let number = num;
+  let number2 = num;
+  let question = '..';
+  const step = randomNumber(1, 10);
+  const leftSideProgression = randomNumber(1, 10);
+  const rightSideProgression = 9 - leftSideProgression;
+
+  for (let i = 0; i < leftSideProgression; i += 1) {
+    number -= step;
+    question = `${number} ${question}`;
+  }
+  for (let i = 0; i < rightSideProgression; i += 1) {
+    number2 += step;
+    question = `${question} ${number2}`;
+  }
+  return question;
+};
+
+export const answer1 = String(randomNumber(1, 100));
+export const question1 = makeQuestion(Number(answer1));
+
+export const answer2 = String(randomNumber(1, 100));
+export const question2 = makeQuestion(Number(answer2));
+
+export const answer3 = String(randomNumber(1, 100));
+export const question3 = makeQuestion(Number(answer3));
